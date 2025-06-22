15befc8875b13d5c35d8348caf1868645dfd1386
Emirkan Burak Yılmaz <emirkan.b.yilmaz@gmail.com>
2025-06-22 22:32:33 +0300

    add assets

---
+++ unity/Assets/Models/DroneGuard/Animation/AnimContr.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimContr : MonoBehaviour
+{
+    public Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnMouseDown()
+    {
+        anim.Play("ShutDown");
+    }
+
+    void OnMouseUp()
+    {
+        anim.Play("WakeUp");
+    }
+
+    void OnMouseDrag()
+    {
+        anim.Play("Destroyed");
+    }
+}
